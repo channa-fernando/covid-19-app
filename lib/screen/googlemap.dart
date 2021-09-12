1e53327ddfe9e7ca51221b439921bc08b85e4cbb
Channa Fernando <fchanna2853@gmail.com>
2021-09-12 18:21:40 +0530

    fixed location issues

--- lib/screen/googlemap.dart
+++ lib/screen/googlemap.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapView extends StatefulWidget {
   const MapView({Key? key}) : super(key: key);
@@ -8,20 +9,40 @@ class MapView extends StatefulWidget {
   _MapViewState createState() => _MapViewState();
 }
 
+
+
 class _MapViewState extends State<MapView> {
 
-  final CameraPosition _initialCameraPosition = CameraPosition(target: LatLng(7.1930961, 80.2648257));
   late GoogleMapController _googleMapController;
+  LatLng currentPosition =  LatLng(7.1930961, 80.2648257);
+
+  @override
+  void initState () {
+    super.initState();
+    _getUserLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        initialCameraPosition: _initialCameraPosition,
+        initialCameraPosition: CameraPosition(
+          target: currentPosition,
+          zoom: 11.0,
+
+        ),
         mapType: MapType.normal,
+        myLocationEnabled: true,
         onMapCreated: (controller){
          setState(() {
            _googleMapController = controller;
+           _googleMapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: currentPosition,zoom: 11.0)));
+           new Marker(
+             icon: BitmapDescriptor.defaultMarker,
+             markerId: MarkerId("currentPosition"),
+             position: currentPosition,
+             infoWindow: InfoWindow(title: "userMarker", snippet: '*'),
+           );
          });
         },
         onTap: (coordinate){
@@ -33,5 +54,15 @@ class _MapViewState extends State<MapView> {
         child: Icon(Icons.zoom_out),
       ),
     );
+  }
+
+  void _getUserLocation() async {
+    var position = await GeolocatorPlatform.instance
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print("Location: " + position.longitude.toString() + " , " +  position.longitude.toString());
+    print(position);
+    setState(() {
+      currentPosition = LatLng(position.latitude, position.longitude);
+    });
   }
 }
